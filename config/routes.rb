6a365dcc3ea2677463021e092ecc74298ab3c07c
Constantin Eulenstein <constantin.eulenstein@t-online.de>
2019-12-03 13:36:57 +0800

    Added show and index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'users/registrations'}
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :locallects, only: [:index, :show, :new, :create, :edit, :update]  do
+  resources :locallects, only: [:index, :show, :new, :create, :edit]  do
     resources :friendships, only: [:index, :show, :new, :create, :destroy] do
       resources :transactions
     end
