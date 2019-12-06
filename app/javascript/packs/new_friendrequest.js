add8bfcdd1bf310a091b33d4f51020fbe4f53b65
Constantin Eulenstein <constantin.eulenstein@t-online.de>
2019-12-06 19:15:55 +0800

    Added fading out of background for transactions

--- app/javascript/packs/new_friendrequest.js
+++ app/javascript/packs/new_friendrequest.js
@@ -8,13 +8,11 @@ const displayExtraField = (e) => {
     e.style.backgroundImage = "linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7))"
   })
 
-  document.querySelector(".cover-by-popup-banner").style.backgroundImage = `linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)), url(${user.city_image})`;
-  if (document.querySelector(".cover-by-popup-image")){
-    document.querySelector(".cover-by-popup-image").style.backgroundImage =  `linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)), url(${user.photo.url})`;
-  }
-
   if (document.querySelector(".cover-by-popup-image-book")){
     document.querySelector(".cover-by-popup-image-book").style.backgroundImage =  `linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)), url(${chatter.photo.url})`;
+  } else {
+    document.querySelector(".cover-by-popup-image").style.backgroundImage =  `linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)), url(${user.photo.url})`;
+    document.querySelector(".cover-by-popup-banner").style.backgroundImage = `linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)), url(${user.city_image})`;
   }
 }
 
