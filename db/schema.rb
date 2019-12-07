12e5b008c9d1817b713db5acce81ea573c1879e4
Constantin Eulenstein <constantin.eulenstein@t-online.de>
2019-12-07 13:23:37 +0800

    transaction confirmation integrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_05_072241) do
+ActiveRecord::Schema.define(version: 2019_12_07_044542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -141,6 +141,8 @@ ActiveRecord::Schema.define(version: 2019_12_05_072241) do
     t.bigint "explorer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "approved"
+    t.boolean "declined"
     t.index ["explorer_id"], name: "index_transactions_on_explorer_id"
     t.index ["locallect_id"], name: "index_transactions_on_locallect_id"
   end
