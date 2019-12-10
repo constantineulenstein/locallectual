695711159ec2ecc31646abf680c1fc1aeb7f99e4
Constantin Eulenstein <constantin.eulenstein@t-online.de>
2019-12-10 12:13:51 +0800

    forum backend set up

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -16,7 +16,9 @@ class ForumsController < ApplicationController
     else
       @forums = policy_scope(Forum).all
     end
-
+    @forums = @forums.order(created_at: :desc)
+    # For new post
+    @forum = Forum.new
     @languages = ['Abkhaz',
                   'Afar',
                   'Afrikaans',
@@ -207,14 +209,12 @@ class ForumsController < ApplicationController
     authorize @forum
   end
 
-  def new
-    @forum = Forum.new
-  end
-
   def create
     @forum = Forum.create(forum_params)
+    @forum.user_id = current_user.id
     @forum.save
-    redirect_to forum_path(@forum)
+    redirect_to forums_path
+    authorize @forum
   end
 
 
@@ -228,6 +228,6 @@ class ForumsController < ApplicationController
   private
 
   def forum_params
-    params.require(:forum).permit(:title, :description)
+    params.require(:forum).permit(:title, :description, :location)
   end
 end
--- app/javascript/packs/new_friendrequest.js
+++ app/javascript/packs/new_friendrequest.js
@@ -6,10 +6,20 @@ const displayExtraField = (e) => {
   const requestField = document.querySelector('.friend-request-field');
   requestField.style.display = "block";
 
+  const specificSearchField = document.querySelectorAll('.select2-selection.select2-selection--multiple')[0];
+  if (specificSearchField) {
+    specificSearchField.style.backgroundImage = "linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7))"
+  };
+
   // check if transaction page, if yes than do autofill
   const addressInput3 = document.getElementById('transaction_location');
   if (addressInput3) {
     places({ container: addressInput3 });
+  };
+
+  const addressInput4 = document.getElementById('forum_location');
+  if (addressInput4) {
+    places({ container: addressInput4 });
   };
 
 
@@ -20,7 +30,7 @@ const displayExtraField = (e) => {
 
   if (document.querySelector(".cover-by-popup-image-book")){
     document.querySelector(".cover-by-popup-image-book").style.backgroundImage =  `linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)), url(${chatter.photo.url})`;
-  } else {
+  } else if (document.querySelector(".cover-by-popup-image")) {
     document.querySelector(".cover-by-popup-image").style.backgroundImage =  `linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)), url(${user.photo.url})`;
     document.querySelector(".cover-by-popup-banner").style.backgroundImage = `linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)), url(${user.city_image})`;
   }
