bec18152dce16f96f714e5ea58154922e7faef4d
Constantin Eulenstein <constantin.eulenstein@t-online.de>
2019-12-11 16:19:21 +0800

    Added seed and comment order

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_one :explorer, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_many :forums, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
