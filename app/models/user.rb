89eb109323786321021e22a045aa6ca2a3b24d26
Jeffrey Yeung <jeff.yeung23@gmail.com>
2019-12-12 16:09:27 +0800

    Fixed agolia font color, fixed homepage picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ApplicationRecord
   after_update :get_city_img_url, if: :saved_change_to_base_location?
   after_update :get_city_img_url, if: :saved_change_to_seek_location?
   # before_update :get_city_img_url, if: :base_location_changed?
+
   after_update :calculate_age
 
   # Utilizing pg_search for searching baselocation of locallects
