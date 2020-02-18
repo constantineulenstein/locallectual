282dd77642b92ea7abe79ac509d9c4b55deb0f2b
Constantin Eulenstein <constantin.eulenstein@t-online.de>
2020-02-18 15:31:53 +0100

    gender search does now work in multiple languages

--- app/policies/locallect_policy.rb
+++ app/policies/locallect_policy.rb
@@ -5,6 +5,10 @@ class LocallectPolicy < ApplicationPolicy
     end
   end
 
+  def index?
+    true
+  end
+
   def show?
     true
   end
--- config/application.rb
+++ config/application.rb
@@ -16,9 +16,11 @@ module Locallectual
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.active_job.queue_adapter = :sidekiq
+
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
     I18n.available_locales = [:en, :de, :fr]
     config.i18n.default_locale = :en
+    config.i18n.fallbacks = [I18n.default_locale]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
